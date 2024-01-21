317115acb48bd762a4d3e53a25318942ab8b1b1f
codeelk <chaomengop@163.com>
2024-01-21 14:29:52 +0800

    ET准备工作后的代码

---
+++ Server/Hotfix/Demo/Unit/UnitFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ET
+{
+    public static class UnitFactory
+    {
+        public static Unit Create(Scene scene, long id, UnitType unitType)
+        {
+            UnitComponent unitComponent = scene.GetComponent<UnitComponent>();
+            switch (unitType)
+            {
+                case UnitType.Player:
+                {
+                    Unit unit = unitComponent.AddChildWithId<Unit, int>(id, 1001);
+                    //ChildType测试代码 取消注释 编译Server.hotfix 可发现报错
+                    //unitComponent.AddChild<Player, string>("Player");
+                    unit.AddComponent<NumericComponent>();
+                    
+                    unitComponent.Add(unit);
+                    return unit;
+                }
+                default:
+                    throw new Exception($"not such unit type: {unitType}");
+            }
+        }
+    }
+}
---
+++ Unity/Codes/Model/Demo/EventType.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+
+namespace ET
+{
+    namespace EventType
+    {
+        public struct AppStart
+        {
+        }
+
+        public struct SceneChangeStart
+        {
+            public Scene ZoneScene;
+        }
+        
+        
+        public struct SceneChangeFinish
+        {
+            public Scene ZoneScene;
+            public Scene CurrentScene;
+        }
+
+
+        
+        public class ChangePosition: DisposeObject
+        {
+            public static readonly ChangePosition Instance = new ChangePosition();
+                    
+            public Unit Unit;
+            public WrapVector3 OldPos = new WrapVector3();
+
+            // 因为是重复利用的，所以用完PublishClass会调用Dispose
+            public override void Dispose()
+            {
+                this.Unit = null;
+            }
+        }
+ 
+
+        public class ChangeRotation: DisposeObject
+        {
+            public static readonly ChangeRotation Instance = new ChangeRotation();
+            
+            public Unit Unit;
+        
+               // 因为是重复利用的，所以用完PublishClass会调用Dispose
+            public override void Dispose()
+            {
+                this.Unit = null;
+            }
+        }
+
+        public struct PingChange
+        {
+            public Scene ZoneScene;
+            public long Ping;
+        }
+        
+        public struct AfterCreateZoneScene
+        {
+            public Scene ZoneScene;
+        }
+        
+        public struct AfterCreateCurrentScene
+        {
+            public Scene CurrentScene;
+        }
+        
+        public struct AfterCreateLoginScene
+        {
+            public Scene LoginScene;
+        }
+
+        public struct AppStartInitFinish
+        {
+            public Scene ZoneScene;
+        }
+
+        public struct LoginFinish
+        {
+            public Scene ZoneScene;
+        }
+
+        public struct LoadingBegin
+        {
+            public Scene Scene;
+        }
+
+        public struct LoadingFinish
+        {
+            public Scene Scene;
+        }
+
+        public struct EnterMapFinish
+        {
+            public Scene ZoneScene;
+        }
+
+        public struct AfterUnitCreate
+        {
+            public Unit Unit;
+        }
+        
+        public struct MoveStart
+        {
+            public Unit Unit;
+        }
+
+        public struct MoveStop
+        {
+            public Unit Unit;
+        }
+
+        public struct UnitEnterSightRange
+        {
+        }
+        
+        public struct StartGameLevel
+        {
+            public Scene ZoneScene;
+        }
+        
+        public struct AdventureBattleRound
+        {
+            public Scene ZoneScene;
+            public Unit AttackUnit;
+            public Unit TargetUnit;
+        }
+        public struct AdventureBattleRoundView
+        {
+            public Scene ZoneScene;
+            public Unit AttackUnit;
+            public Unit TargetUnit;
+        }
+
+
+        public struct AdventureBattleOver
+        {
+            public Scene ZoneScene;
+            public Unit WinUnit;
+        }
+        
+        public struct ExpChange
+        {
+            public Scene ZoneScene;
+            public bool isEnoughUpLevel;
+        }
+        
+        public struct MakeQueueOver
+        {
+            public Scene ZoneScene;
+        }
+        
+        public struct UpdateTaskInfo
+        {
+            public Scene ZoneScene;
+        }
+        
+        public struct UpdateChatInfo
+        {
+            public Scene ZoneScene;
+        }
+        
+    }
+}
